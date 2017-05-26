1c617bad0d4d453f518a3df76184fc56cf2839d6
Ueli Hofstetter <uelihofstetter@gmail.com>
2017-05-26 18:04:52 +0200

    added test

---
+++ src/test/java/ch/blackbird/MainTest.java
@@ -0,0 +1,15 @@
+package ch.blackbird;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MainTest {
+
+	@Test
+	public void sampeTest() {
+		assertTrue(true);
+		//fail("Not yet implemented");
+	}
+
+}
